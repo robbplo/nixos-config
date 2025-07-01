b1168118bf7a845f463c7d91b412590527a7595e
Robbin Ploeger <robbplo@gmail.com>
2025-07-01 11:51:21 +0200

    symlink nvim config and configure noice plugin

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   programs.fzf.enable = true;
@@ -29,28 +29,23 @@
       stylelint
       stylua
     ];
-    extraConfig = ''
-      luafile ${./lua/packer_init.lua}
-
-      luafile ${./lua/core/autocmds.lua}
-      luafile ${./lua/core/colors.lua}
-      luafile ${./lua/core/options.lua}
-      luafile ${./lua/core/keymap.lua}
-
-      luafile ${./lua/plugins/barbar.lua}
-      luafile ${./lua/plugins/debugger.lua}
-      luafile ${./lua/plugins/gitsigns.lua}
-      luafile ${./lua/plugins/godot.lua}
-      luafile ${./lua/plugins/indent-blankline.lua}
-      luafile ${./lua/plugins/lualine.lua}
-      luafile ${./lua/plugins/neovide.lua}
-      luafile ${./lua/plugins/noice.lua}
-      luafile ${./lua/plugins/nvim-cmp.lua}
-      luafile ${./lua/plugins/nvim-lspconfig.lua}
-      luafile ${./lua/plugins/nvim-telescope.lua}
-      luafile ${./lua/plugins/nvim-treesitter.lua}
-      luafile ${./lua/plugins/oil.lua}
-      luafile ${./lua/plugins/trouble.lua}
-    '';
   };
+  home.file.".config/nvim/init.lua".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home/nvim/init.lua";
+  home.file.".config/nvim/lua" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home/nvim/lua";
+    recursive = true;
+  };
+  home.file.".config/nvim/after" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home/nvim/after";
+    recursive = true;
+  };
+  # home.file.".config/nvim/lua" = {
+  #   source = ./lua;
+  #   recursive = true;
+  # };
+  # home.file.".config/nvim/after" = {
+  #   source = ./after;
+  #   recursive = true;
+  # };
 }
