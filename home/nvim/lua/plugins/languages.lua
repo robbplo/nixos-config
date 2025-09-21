1bfadff1060389a45299a8e6c977e72f1d3715aa
Robbin Ploeger <robbplo@gmail.com>
2025-09-21 12:48:19 +0200

    Replace neodev with lazydev

--- home/nvim/lua/plugins/languages.lua
+++ home/nvim/lua/plugins/languages.lua
@@ -1,9 +1,12 @@
 return {
-	{ 'folke/neodev.nvim' },
-	{ 'jwalton512/vim-blade' },
-	{ "fladson/vim-kitty" },
-	{ "dag/vim-fish" },
-	{ "joerdav/templ.vim" },
-	{ "gpanders/nvim-parinfer" },
-	{ "habamax/vim-godot" },
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua'
+  },
+  { 'jwalton512/vim-blade' },
+  { "fladson/vim-kitty" },
+  { "dag/vim-fish" },
+  { "joerdav/templ.vim" },
+  { "gpanders/nvim-parinfer" },
+  { "habamax/vim-godot" },
 }
